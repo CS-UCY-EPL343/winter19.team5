7f0f78b6b4e9e963c54480bc94ff129673760e42
mpafit02 <mpafit02@cs.ucy.ac.cy>
2019-11-24 19:01:20 +0200

    Dropdown Implementation, Add to Journey basic Implementation

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-  function addToJourney() {
-      // Add to Journey
-  }
-
   function addToLikes() {
       // Add to Likes
   }
 
   function addToDislikes() {
       // Add to Dislikes
+  }
+
+  function hideJourney(id) {
+      var x = document.getElementById(id);
+      x.hidden = true;
+  }
+
+  function addToJourney(id) {
+      var x = document.getElementById(id);
+      x.hidden = false;
   }
