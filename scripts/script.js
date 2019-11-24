3d723abc9c72d05b5a018bf0b98287ca0a21a3e0
vpariz01 <vpariz01@users.noreply.github.com>
2019-11-24 23:05:18 +0200

    A new dialog box

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,44 @@
-function hideJourney(id) {
-    document.getElementById(id).hidden = true;
-}
 
-function addToJourney(id) {
-    if (document.getElementById(id).hidden === false) {
-        console.log("Exists");
-    } else {
-        document.getElementById(id).hidden = false;
-    }
-}
-
-function CustomAlert() {
-    this.render = function (dialog) {
+function CustomAlert(){
+    this.render = function(dialog){
         var winW = window.innerWidth;
         var winH = window.innerHeight;
         var dialogoverlay = document.getElementById('dialogoverlay');
         var dialogbox = document.getElementById('dialogbox');
         dialogoverlay.style.display = "block";
-        dialogoverlay.style.height = winH + "px";
-        dialogbox.style.left = (winW / 2) - (550 * .5) + "px";
+        dialogoverlay.style.height = winH+"px";
+        dialogbox.style.left = (winW/2) - (550 * .5)+"px";
         dialogbox.style.top = "100px";
         dialogbox.style.display = "block";
         document.getElementById('dialogboxhead').innerHTML = "Acknowledge This Message";
         document.getElementById('dialogboxbody').innerHTML = dialog;
         document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
     }
-    this.ok = function () {
-        document.getElementById('dialogbox').style.display = "none";
-        document.getElementById('dialogoverlay').style.display = "none";
+	this.ok = function(){
+		document.getElementById('dialogbox').style.display = "none";
+		document.getElementById('dialogoverlay').style.display = "none";
+	}
+}
+var Alert = new CustomAlert();
+
+
+
+function hideJourney(id) {
+	
+    document.getElementById(id).hidden = true;
+}
+
+function addToJourney(id) {
+    if (document.getElementById(id).hidden === false) {
+		Alert.render('You have already added the food.');
+        console.log("Exists");
+    } else {
+		Alert.render('You have added the food succesfully.');
+        document.getElementById(id).hidden = false;
     }
 }
 
-var Alert = new CustomAlert();
+
 
 function hideElement(id) {
     Swal.fire({
