de66b93d58ffeed297ade70820c9d94108c0e14c
mpafit02 <mpafit02@cs.ucy.ac.cy>
2019-11-24 21:54:35 +0200

    Basic collapse mechanism

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-  function addToLikes() {
-      // Add to Likes
-  }
-
-  function addToDislikes() {
-      // Add to Dislikes
-  }
-
   function hideJourney(id) {
-      var x = document.getElementById(id);
-      x.hidden = true;
+      document.getElementById(id).hidden = true;
   }
 
-  function addToJourney(id) {
+  function addToJourney(id, id1, id2) {
       var x = document.getElementById(id);
       x.hidden = false;
+      var x1 = document.getElementById(id1);
+      x1.hidden = true;
+      var x2 = document.getElementById(id2);
+      x2.hidden = true;
   }
---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+  function addToLikes() {
+      // Add to Likes
+  }
+
+  function addToDislikes() {
+      // Add to Dislikes
+  }
+
+  function hideJourney(id) {
+      var x = document.getElementById(id);
+      x.hidden = true;
+  }
+
+  function addToJourney(id) {
+      var x = document.getElementById(id);
+      x.hidden = false;
+  }
