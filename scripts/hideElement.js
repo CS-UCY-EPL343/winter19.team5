95e147544987be4dca1c053bdc167912b90f8b56
vpariz01 <vpariz01@users.noreply.github.com>
2019-11-24 20:27:41 +0200

    Updated the hideElement script

--- scripts/hideElement.js
+++ scripts/hideElement.js
@@ -6,6 +6,18 @@
 
 function hideElement(id)
 {
-	var temp='#'+id;
-	$(temp).hide();
+	
+	if (confirm('Are you sure you want to save this thing into the database?')) {
+		
+		var temp='#'+id;
+		$(temp).hide();
+	} else {
+    // Do nothing!
+	}
+	
+	
+	
+	prompt("Please enter your name", "Harry Potter");
+	
+	
 }
