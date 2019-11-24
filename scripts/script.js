2183bd1fb5dc637c05df35d2f0104ee3009f7984
mpafit02 <mpafit02@cs.ucy.ac.cy>
2019-11-24 22:45:38 +0200

    Add sweer alerts for deletion of Likes and Dislikes

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,51 @@
-  function addToLikes() {
-      // Add to Likes
-  }
+function hideJourney(id) {
+    document.getElementById(id).hidden = true;
+}
 
-  function addToDislikes() {
-      // Add to Dislikes
-  }
+function addToJourney(id) {
+    document.getElementById(id).hidden = false;
+}
 
-  function hideJourney(id) {
-      var x = document.getElementById(id);
-      x.hidden = true;
-  }
+function CustomAlert() {
+    this.render = function (dialog) {
+        var winW = window.innerWidth;
+        var winH = window.innerHeight;
+        var dialogoverlay = document.getElementById('dialogoverlay');
+        var dialogbox = document.getElementById('dialogbox');
+        dialogoverlay.style.display = "block";
+        dialogoverlay.style.height = winH + "px";
+        dialogbox.style.left = (winW / 2) - (550 * .5) + "px";
+        dialogbox.style.top = "100px";
+        dialogbox.style.display = "block";
+        document.getElementById('dialogboxhead').innerHTML = "Acknowledge This Message";
+        document.getElementById('dialogboxbody').innerHTML = dialog;
+        document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
+    }
+    this.ok = function () {
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+    }
+}
 
-  function addToJourney(id) {
-      var x = document.getElementById(id);
-      x.hidden = false;
-  }
+var Alert = new CustomAlert();
+
+function hideElement(id) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.value) {
+            $("#" + id).hide();
+            Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            )
+        }
+    })
+}
