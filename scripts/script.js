02812f35ec479f42fd3d55f558132c137095f58b
mpafit02 <mpafit02@cs.ucy.ac.cy>
2019-11-24 23:11:22 +0200

    Add sweet alerts for Add to Journey

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,56 @@
-
-function CustomAlert(){
-    this.render = function(dialog){
-        var winW = window.innerWidth;
-        var winH = window.innerHeight;
-        var dialogoverlay = document.getElementById('dialogoverlay');
-        var dialogbox = document.getElementById('dialogbox');
-        dialogoverlay.style.display = "block";
-        dialogoverlay.style.height = winH+"px";
-        dialogbox.style.left = (winW/2) - (550 * .5)+"px";
-        dialogbox.style.top = "100px";
-        dialogbox.style.display = "block";
-        document.getElementById('dialogboxhead').innerHTML = "Acknowledge This Message";
-        document.getElementById('dialogboxbody').innerHTML = dialog;
-        document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
-    }
-	this.ok = function(){
-		document.getElementById('dialogbox').style.display = "none";
-		document.getElementById('dialogoverlay').style.display = "none";
-	}
-}
-var Alert = new CustomAlert();
-
+// function CustomAlert() {
+//     this.render = function (dialog) {
+//         var winW = window.innerWidth;
+//         var winH = window.innerHeight;
+//         var dialogoverlay = document.getElementById('dialogoverlay');
+//         var dialogbox = document.getElementById('dialogbox');
+//         dialogoverlay.style.display = "block";
+//         dialogoverlay.style.height = winH + "px";
+//         dialogbox.style.left = (winW / 2) - (550 * .5) + "px";
+//         dialogbox.style.top = "100px";
+//         dialogbox.style.display = "block";
+//         document.getElementById('dialogboxhead').innerHTML = "Acknowledge This Message";
+//         document.getElementById('dialogboxbody').innerHTML = dialog;
+//         document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
+//     }
+//     this.ok = function () {
+//         document.getElementById('dialogbox').style.display = "none";
+//         document.getElementById('dialogoverlay').style.display = "none";
+//     }
+// }
+// var Alert = new CustomAlert();
 
 
 function hideJourney(id) {
-	
+
     document.getElementById(id).hidden = true;
 }
 
 function addToJourney(id) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        onOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
     if (document.getElementById(id).hidden === false) {
-		Alert.render('You have already added the food.');
-        console.log("Exists");
+        Toast.fire({
+            icon: 'error',
+            title: 'Product already in the Journey'
+        });
     } else {
-		Alert.render('You have added the food succesfully.');
+        Toast.fire({
+            icon: 'success',
+            title: 'Added successfully'
+        });
         document.getElementById(id).hidden = false;
     }
 }
-
-
 
 function hideElement(id) {
     Swal.fire({
