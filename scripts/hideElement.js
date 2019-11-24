30fd4a5dee8417ff867ee89dedd0c039d03378fd
vpariz01 <vpariz01@users.noreply.github.com>
2019-11-24 20:29:40 +0200

    Updated the hideElement  script with a confirmation message

--- scripts/hideElement.js
+++ scripts/hideElement.js
@@ -7,7 +7,7 @@
 function hideElement(id)
 {
 	
-	if (confirm('Are you sure you want to save this thing into the database?')) {
+	if (confirm('Are you sure you want to discrad this Food from your list?')) {
 		
 		var temp='#'+id;
 		$(temp).hide();
